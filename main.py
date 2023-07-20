0660b0e90924a42adfd42bff29f9e4cc253faafe
Sreyas sai <91191228+Sreyaaas@users.noreply.github.com>
2023-07-20 23:29:24 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+import discord
+import os
+import openai
+
+with open("chat.txt","r") as f:
+  chat = f.read()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+token = os.getenv("SECRET_KEY")
+
+
+class MyClient(discord.Client):
+
+  async def on_ready(self):
+    print(f'Logged on as {self.user}!')
+    
+  async def on_message(self, message):
+    global chat
+    chat += f"{message.author}: {message.content}\n"
+    print(f'Message from {message.author}: {message.content}')
+    if self.user != message.author:
+     if self.user in message.mentions:
+       print(chat)
+       response = openai.Completion.create(        
+         model="text-davinci-003",
+         prompt=f"{chat}\nYashGPT:",
+         temperature=1,
+         max_tokens=80,
+         top_p=1,
+         frequency_penalty=0,
+         presence_penalty=0
+      )
+     
+       channel = message.channel
+       messageToSend = response.choices[0].text
+       await channel.send(messageToSend )
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run(token)
